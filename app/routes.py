2ae9d756d5d31c69b290c2c9b175e2af66dd7a43
linothomas14 <linolowlevel@gmail.com>
2022-05-12 12:09:33 +0700

    initial init for project

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,21 @@
+from crypt import methods
+from flask import jsonify
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return jsonify({"msg" : "This is price-optimizer-api"})
+
+@app.route('/users', methods = ['GET'])
+def user_list():
+    return jsonify({"msg" : "Test user"})
+
+@app.route('/users/<int:city_id>', methods = ['GET'])
+def user_list():
+    return jsonify({"msg" : "Test user"})
+
+@app.route('/users', methods = ['POST'])
+def register_user():
+    return jsonify({"msg": "Success"})
