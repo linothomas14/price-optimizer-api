9328023c5fe6fee94de9f11e931b22a44cd6f5e2
linothomas14 <linolowlevel@gmail.com>
2022-05-13 09:28:43 +0700

    add Transaction Models and many more ..

--- app/models.py
+++ app/models.py
@@ -2,13 +2,96 @@ from app import db
 from datetime import datetime
 
 
-class Users(db.Model):
-    id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(230), nullable=False)
     email = db.Column(db.String(120), index=True, unique=True, nullable=False)
-    password = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow)
+    transactions = db.relationship('Transaction', backref='author', lazy='dynamic')
+
+    def __init__(self, name, email, password_hash, created_at, updated_at):
+        self.name = name
+        self.email = email
+        self.password_hash = password_hash
+        self.created_at = created_at
+        self.updated_at = updated_at
 
     def __repr__(self):
         return '<User {}>'.format(self.name)
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+        
+    def getAll():
+        users = User.query.all()
+        result = []
+        for user in users:
+            obj = {
+                'id': user.id,
+                'name': user.name,
+                'email': user.email,
+            }
+            result.append(obj)
+        return result
+    
+    def getById():
+        """
+            Get user by id
+        """
+        pass 
+
+    def update():
+        """
+            Update user by id
+        """
+        pass
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+class Transaction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    product = db.Column(db.String(140))
+    price = db.Column(db.Integer)
+    timestamp = db.Column(db.DateTime,index=True, default=datetime.utcnow)
+    transaction_type = db.Column(db.String(140))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __init__(self, product, price, timestamp, user_id, transaction_type):
+        self.product = product
+        self.price = price
+        self.timestamp = timestamp
+        self.transaction_type = transaction_type
+        self.user_id = user_id
+        
+
+    def __repr__(self):
+        return '<id_transaction: {},user_id: {}>'.format(self.id,self.user_id)
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+        
+    def getAll():
+        pass
+    
+    def getById():
+        """
+            Get user by id
+        """
+        pass 
+
+    def update():
+        """
+            Update user by id
+        """
+        pass
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from crypt import methods
-from flask import jsonify
-from app import app
+from flask import jsonify,request
+from app import app,db
+from app.models import User
 
 
 @app.route('/')
@@ -8,14 +9,38 @@ from app import app
 def index():
     return jsonify({"msg" : "This is price-optimizer-api"})
 
-@app.route('/users', methods = ['GET'])
-def user_list():
-    return jsonify({"msg" : "Test user"})
+@app.route('/user', methods = ['GET','POST'])
+def user():
+    if request.method == 'GET':
+        users= User.query.all()
+        result = []
+        for user in users:
+            obj = {
+                'name' : user.name,
+                'email' : user.email,
+                'method' : request.method
+            }   
+            result.append(obj)
+        return jsonify(result)
 
-@app.route('/users/<int:city_id>', methods = ['GET'])
-def user_list():
-    return jsonify({"msg" : "Test user"})
+    elif request.method == 'POST':
+        
+        """
+        Ngambil data dari JSON parse ke db.session.add(u) -> db.session.commit()
+        baru return success
+        """
+        return jsonify({"msg": "Success"})
 
-@app.route('/users', methods = ['POST'])
-def register_user():
-    return jsonify({"msg": "Success"})
+
+@app.route('/user/<int:id>', methods = ['GET'])
+def user_get(id):
+    user_id = id
+    u = User.query.get(user_id)
+    construct = {
+            'user': {
+                'id': u.id,
+                'name': u.name,
+            }
+        }
+
+    return jsonify(construct)
