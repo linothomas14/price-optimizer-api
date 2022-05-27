a1ddf790e1e971dc357cb71b95ffa94bc62d2145
linothomas14 <linolowlevel@gmail.com>
2022-05-27 15:10:54 +0700

    feat-paging for users and products entity

--- app/routes/product.py
+++ app/routes/product.py
@@ -8,12 +8,20 @@ from app.routes.auth import checkAuth
 @app.route('/products', methods = ['GET','POST'])
 def product():
     if request.method == 'GET':
-        return ProductController.index()
+        page = request.args.get('page', 1)
+        return ProductController.index(page)
     else :
         isAdmin = checkAuth()
         if isAdmin is False:
             return response.badRequest('','auth not recognized')
         return ProductController.addProduct()
+
+# @app.route('/products?=page', methods = ['GET'])
+# def getProductPagination(page):
+#     isAdmin = checkAuth()
+#     if isAdmin is False:
+#         return response.badRequest('','auth not recognized')
+#     return ProductController.index(page)
 
 # CRUD product
 @app.route('/products/<int:id>', methods = ['GET','PUT', 'DELETE'])
