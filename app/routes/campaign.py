387d9a4e2e4c91974d874a0ae067a24c920de15c
manfred michael <koskosanrider@gmail.com>
2022-06-04 00:35:10 +0700

    Add routing for demand forecasting

--- app/routes/campaign.py
+++ app/routes/campaign.py
@@ -29,5 +29,5 @@ def apply_campaign():
     return CampaignController.applyCampaign()
 
 @app.route('/campaigns/predict-demand', methods=['GET'])
-def apply_campaign():
+def predict_demand():
     return CampaignController.predictDemand()
